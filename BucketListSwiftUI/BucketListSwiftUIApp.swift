b363d217a4a4e17b03fb7f81a9326a5f17c2dd43
Ramill <ram4ik@hotmail.com>
2024-01-08 18:56:24 +0200

    Initial Commit

---
+++ BucketListSwiftUI/BucketListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListSwiftUIApp.swift
+//  BucketListSwiftUI
+//
+//  Created by admin on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
